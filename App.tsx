b02d0abac84e6ea8f27c7a15f0ca1d1a1deecb3f
Ruteski <lruteski@gmail.com>
2022-12-28 18:58:29 -0300

    estilizando os elementos

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default function App(){
+   return(
+      <View style={{
+         flex: 1,
+         backgroundColor: '#131016',
+         padding: 30
+      }}>
+         <Text style={{
+            color: '#fdfcfe',
+            fontSize: 24,
+            fontWeight: 'bold',
+            marginTop: 48
+         }}>
+            Nome do evento
+         </Text>
+
+         <Text style={{
+            color: '#6b6b6b',
+            fontSize: 16,
+         }}>
+            Sexta, 30 de dezembro de 2022.
+         </Text>
+      </View>
+   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
