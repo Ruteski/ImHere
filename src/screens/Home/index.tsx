91692701ae71fdcc50cb23de5b7a96d5f39c3049
Ruteski <lruteski@gmail.com>
2022-12-29 10:53:44 -0300

    adicionado scrollview na lista de participantes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
-import {Text, View, TextInput, TouchableOpacity} from "react-native";
+import {Text, View, TextInput, TouchableOpacity, ScrollView} from "react-native";
 
 import {Participant} from "../../components/Participant";
 
 import {styles} from "./styles";
 
 export default function Home(){
+   const participants = ['Lincoln', 'Maisa', 'Noah', 'Mélanie', 'Anthony', 'Donathelo', 'Anthonela', 'Lilith',
+      'Charlothe', 'Goku', 'Goha', 'Picollo'];
    const handleParticipantAdd = () => {
       console.log('vc clicou no botao de adicionar!');
    }
@@ -40,10 +42,20 @@ export default function Home(){
             </TouchableOpacity>
          </View>
 
-         <Participant
-            name="Lincoln"
-            onRemove={handleParticipantRemove}
-         />
+         <ScrollView
+            showsVerticalScrollIndicator={false}
+         >
+            {
+               participants.map(participant => (
+                  <Participant
+                     key={participant}
+                     name={participant}
+                     onRemove={handleParticipantRemove}
+                  />
+               ))
+            }
+         </ScrollView>
+
 
       </View>
    )
